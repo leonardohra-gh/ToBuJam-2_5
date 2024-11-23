fd2c08dc3b88898db4dd71caac93e058d897cdd2
L1MAS <luana3.limas@gmail.com>
2024-11-23 13:24:36 -0300

    Ajustes casa, jogador e rua

--- entitiesGame/rua.lua
+++ entitiesGame/rua.lua
@@ -2,15 +2,14 @@ local Entity = require('core.entity')
 local Rua = Entity:extend()
 local ShapeTypes = require("core.enums.shape_types")
 local BodyTypes = require("core.enums.body_types")
-local MyEntity = Entity:extend()
 
 function Rua:new(x, y)
     local imagePath = "assets/rua.png"
-    MyEntity.super.new(self, x, y, imagePath, World, ShapeTypes.RECTANGLE, BodyTypes.STATIC)
+    Rua.super.new(self, x, y, imagePath, World, ShapeTypes.RECTANGLE, BodyTypes.STATIC)
 end
 
-function Rua:update()
-    self.super.update(self)
+function Rua:update(dt)
+    Rua.super.update(self, dt)
 end
 
 return Rua
