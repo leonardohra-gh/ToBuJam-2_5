99fa35527be35295711f5cdfbc774f1ebcf3ef36
Leonardo Henrique da Rocha Araujo <leonardohra.gh@gmail.com>
2024-11-23 16:48:05 -0300

    Preparação do chão escorregadio

--- entitiesGame/rua.lua
+++ entitiesGame/rua.lua
@@ -3,13 +3,19 @@ local Rua = Entity:extend()
 local ShapeTypes = require("core.enums.shape_types")
 local BodyTypes = require("core.enums.body_types")
 
-function Rua:new(x, y)
+function Rua:new(x, y, rotated)
     local imagePath = "assets/rua.png"
-    Rua.super.new(self, x, y, imagePath, World, ShapeTypes.RECTANGLE, BodyTypes.STATIC)
+    local atravessavel = true
+    self.rotated = rotated
+    Rua.super.new(self, x, y, imagePath, World, ShapeTypes.RECTANGLE, BodyTypes.STATIC, "rua", atravessavel)
 end
 
 function Rua:update(dt)
     Rua.super.update(self, dt)
+end
+
+function Rua:draw()
+    Rua.super.draw(self, self.rotated)
 end
 
 return Rua
