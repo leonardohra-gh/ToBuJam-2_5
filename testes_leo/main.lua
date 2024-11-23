ac650d4734b2bcb70f7975f20f675445e3bc15dd
Salevieno <salvio_almeida@hotmail.com>
2024-11-23 18:22:11 -0300

    Correção testes de leo

--- testes_leo/main.lua
+++ testes_leo/main.lua
@@ -1,7 +1,7 @@
 require("core.auxiliary.world_functions")
 require("core.auxiliary.utils")
 require("core.auxiliary.debug")
-Mochila = require("entitiesGame.jogador")
+Jogador = require("entitiesGame.jogador")
 Parede = require("entitiesGame.parede")
 Chao = require("entitiesGame.chao")
 ChaoCraquelado = require("entitiesGame.chaoCraquelado")
