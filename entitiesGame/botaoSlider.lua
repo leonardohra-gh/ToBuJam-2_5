3e05720bbb2770ef916192a90c7809e19ae99f75
Salevieno <salvio_almeida@hotmail.com>
2024-12-02 00:35:56 -0300

    Melhorias no botão slider

--- entitiesGame/botaoSlider.lua
+++ entitiesGame/botaoSlider.lua
@@ -3,6 +3,8 @@ local ShapeTypes = require("core.enums.shape_types")
 local Object = require("libs.classic")
 local BotaoSlider = Object:extend()
 
+local imageBarra = love.graphics.newImage("assets/botaoSliderBarra.png")
+local imageCirculo = love.graphics.newImage("assets/botaoSliderCirculo.png")
 
 function BotaoSlider:new(x, y, min, max, width, height, step)
     -- BotaoSlider.super.new(self, x, y, "assets/botaoMochila.png", "assets/botaoMochilaHovered.png", qtdItens, ShapeTypes.RECTANGLE, self.alternarSelecao)
@@ -19,8 +21,7 @@ function BotaoSlider:new(x, y, min, max, width, height, step)
             x = x,
             y = y
         },
-        raio = height / 2,
-        cor = {0, 0, 1}
+        raio = 18
     }
 end
 
@@ -28,8 +29,10 @@ function BotaoSlider:update(dt)
 
     if self:isHovered() and love.mouse.isDown(1) then
         local mouseX, mouseY = love.mouse.getPosition()
-        self.circulo.pos.x = mouseX
+        self.circulo.pos.x = mouseX - self.circulo.raio
+        self.circulo.pos.x = math.max(math.min(self.circulo.pos.x, self.x + self.width - 2 * self.circulo.raio), self.x)
         self.value = self:calcValue()
+        self.value = math.max(math.min(self.value, self.max), self.min)
         updateDificuldade(self.value)
     end
     
@@ -37,22 +40,20 @@ function BotaoSlider:update(dt)
 end
 
 function BotaoSlider:draw()
-    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height, 10, 10)
+    local width = math.max(self.circulo.pos.x + 2 * self.circulo.raio - self.x - 2 * 20, 0)
+    love.graphics.draw(imageBarra, self.x, self.y)
     love.graphics.setColor(self.value / self.max, 1 - self.value / self.max, 0, 1)
-    love.graphics.rectangle("fill", self.x, self.y, self.circulo.pos.x + 2 * self.circulo.raio - self.x, self.height, 10, 10)
-    love.graphics.setColor(self.circulo.cor[1], self.circulo.cor[2], self.circulo.cor[3], 1)
-    love.graphics.circle("fill", self.circulo.pos.x + self.circulo.raio, self.circulo.pos.y + self.circulo.raio, self.circulo.raio)
+    love.graphics.rectangle("fill", self.x + 20, self.y + 6, width, self.height - 12, 4, 4)
     love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.draw(imageCirculo, self.circulo.pos.x, self.circulo.pos.y)
 end
 
 function BotaoSlider:isHovered()
+    local tol = 15
     local mouseX, mouseY = love.mouse.getPosition()
+    mouseX = mouseX - self.circulo.raio
 
-    -- local distMouseCirculo = math.sqrt(math.pow((mouseX - self.circulo.pos.x), 2) + math.pow((mouseY - self.circulo.pos.y), 2))
-
-    -- return distMouseCirculo <= self.circulo.raio
-
-    return self.x <= mouseX and mouseX <= self.x + self.width - 2 * self.circulo.raio and self.y <= mouseY and mouseY <= self.y + self.height
+    return self.x - tol <= mouseX and mouseX <= self.x + self.width - 2 * self.circulo.raio + tol and self.y <= mouseY and mouseY <= self.y + self.height
 end
 
 function BotaoSlider:calcValue()
